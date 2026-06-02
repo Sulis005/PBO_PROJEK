9481bba6cf5a6d24495bf719423da121612c81cf
Akbar Udin <hailakbar2007@gmail.com>
2026-06-02 19:24:10 +0700

    nambah spkiel

--- Helpers/database.cs
+++ Helpers/database.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Npgsql;
 
 namespace PBO_PROJEK.Helpers
 {
-    internal class database
+    public class Koneksi
     {
+        public static NpgsqlConnection GetConnection()
+        {
+            string connString =
+                "Host=localhost;" +
+                "Port=5432;" +
+                "Database=projek_pbo;" +
+                "Username=postgres;" +
+                "Password=Usfur.1975";
+
+            return new NpgsqlConnection(connString);
+        }
     }
 }
